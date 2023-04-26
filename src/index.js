4ea6e6f452c2df685baa8ba38cb39ce45169d5bf
adarsh-shahi <adarshshahi1009@gmail.com>
2023-04-26 18:34:47 +0530

    logs and handled unaccepted endpoints

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import { RestWave, Router } from "restwave";
 import { resendOTP, sendOTP, verifyOTP } from "./controller.js";
 const app = new RestWave();
+
+app.use((req, res, next) => {
+	console.log("Passed throught middleware");
+	next();
+});
+
 const router = new Router();
 
 router.post("/sendotp", sendOTP);
@@ -9,6 +15,12 @@ router.post("/resendotp", resendOTP);
 
 app.use("/v1", router);
 
+app.use((req, res) => {
+	res.json({
+		status: "fail",
+		message: "endpoint not available",
+	});
+});
 app.listen(3000, () => {
 	console.log("server listening on 3000");
 });
